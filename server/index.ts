434cbf79a9145506d5d5742f1b4a7ce56dff32e4
Akshita kaistha <112163803+Akshitakaistha@users.noreply.github.com>
2025-07-01 14:39:29 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -7,6 +7,7 @@ import cors from 'cors';
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+app.disable('etag');
 
 app.use(cors({
   origin: 'http://139.59.22.82:7000', // Allow your deployed frontend
