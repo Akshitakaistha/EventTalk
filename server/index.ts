f1c46be0c0a22fcbd73c8057a656d2bf6748eb45
Akshita kaistha <112163803+Akshitakaistha@users.noreply.github.com>
2025-07-29 12:29:27 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -9,10 +9,10 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.disable('etag');
 
-app.use(cors({
-  origin: 'http://139.59.22.82:7000', // Allow your deployed frontend
-  credentials: true
-}));
+// app.use(cors({
+//   origin: 'http://139.59.22.82:7000', // Allow your deployed frontend
+//   credentials: true
+// }));
 app.use(cors());
 
 app.use((req, res, next) => {
