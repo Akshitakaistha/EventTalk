978972537701abc55263909a385d96bc16c1fecf
Akshita kaistha <112163803+Akshitakaistha@users.noreply.github.com>
2025-07-29 10:27:10 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -68,8 +68,8 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
-  app.listen(5000, '127.0.0.1', () => {
-    console.log("Server running on port 5000");
+  const port = 5001;
+  app.listen(5001, '127.0.0.1', () => {
+    console.log("Server running on port 5001");
   });
 })();
